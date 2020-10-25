543a4fbe30899ed9845d867e97ba1e02056f4a22
TheTercept <66691804+TheTercept@users.noreply.github.com>
2020-10-25 09:06:05 -0400

    changes to updateMain.bash for Foundry 0.7.5
    Updated to support Foundry 0.7.5.  Tested (thanks MrVauxs!) on 0.7.5.
    Partly tested on 0.6.6 but should fully work and will be supported.

--- Plutonium/updateMain.bash
+++ Plutonium/updateMain.bash
@@ -2,18 +2,17 @@
 
 ###############################################################################
 # CONFIGURE THE BELOW SETTINGS ONLY
-# DATA_PATH - The path to the data for your foundry is installed (modules/worlds)
+# DATA_PATH - The path to the data for your foundry is installed (modules/worlds)a
 DATA_PATH="$HOME/foundrydata"
 # INSTALL_PATH - Path where foundry is actually installed to
 INSTALL_PATH="$HOME/foundry/resources/app"
 ###############################################################################
 # DO NOT MODIFY ANYTHING BELOW THIS LINE
-VERSION=0.1.2
+VERSION=0.1.3
 # Version 0.1.0 - Initial Version
 #         0.1.1 - Corrected path in sed command to use variable INSTALL_PATH
 #         0.1.2 - Added compatibility with Foundry 0.7.2+
-SERVER_MOD=$DATA_PATH/Data/modules/plutonium/server/plutonium-backend.js
-# check if foundry is installed
+#         0.1.3 - Added compatibility with Foundry 0.7.5
 
 # FUNCTION(S)
 # readJson - Reads a single json value (expects only one key in the file)
@@ -35,46 +34,24 @@ function readJson {
     echo $VALUE ;
   fi; 
 }
-# vercomp - Version Comparision
-# args 2: version1, version2
-function vercomp {
-    if [[ $1 == $2 ]]
-    then
-        return 0
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            return 1
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            return 2
-        fi
-    done
-    return 0
-}
 
 # BEGIN MAIN PROCESSING
 if [[ -f "$INSTALL_PATH/main.js" ]]
 then
 
-   declare minorSix=0.6.6
    foundryVersion=`readJson $INSTALL_PATH/package.json version`
+   foundryMiniorVersion=`echo ${foundryVersion:2:1}`
+   # Set the SERVER_MOD path based on the version of Foundry
+   if [[ "$foundryMiniorVersion" == "7" ]]
+   then
+      # Foundry is version 0.7.x
+      SERVER_MOD=$DATA_PATH/Data/modules/plutonium/server/0.7.x/plutonium-backend.js
+   else
+      # Foundry is version 0.6.x
+      SERVER_MOD=$DATA_PATH/Data/modules/plutonium/server/0.6.x/plutonium-backend.js
+   fi
    
+   # Determine if the backend is already modded, if not, make the mod
    if grep -q "plutonium" $INSTALL_PATH/main.js
    then
       echo "Plutonium edit found"
@@ -82,17 +59,16 @@ then
       cp $INSTALL_PATH/main.js $INSTALL_PATH/main.js.bak
       echo "Plutonium edit not found, making edit"
       
-      `vercomp $minorSix $foundryVersion`
-      result=$?
-      
-      if [[ $result -eq 2 ]] # If Foundry Version is larger than 0.6.6
+      if [[ "$foundryMiniorVersion" == "7" ]]
       then
-         #echo "DEBUG: FOUNDRY IS LARGER THAN 0.6.6"
+         # Foundry is version 0.7.x
          sed 's/require("init")(process.argv, global.paths, startupMessages);/require("init")(process.argv, global.paths, startupMessages).then(() => { require("plutonium-backend").init(); });/g' $INSTALL_PATH/main.js -i
       else
+         # Foundry is version 0.6.x
          sed 's/require("init")(process.argv, global.paths, initLogging);/require("init")(process.argv, global.paths, initLogging).then(() => { require("plutonium-backend").init(); });/g' $INSTALL_PATH/main.js -i
       fi
    fi
+
    # Check if the backed file exists, if not there is an error
    if [[ -f "$SERVER_MOD" ]]
    then
